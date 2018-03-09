fac12bc94d13241992af680a4a08c0357d769e8a
Lyndsy Simon <lyndsy@lyndsysimon.com>
2018-03-09 15:57:36 -0800

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='progeny',
-    version='0.0.1',
+    version='0.1.0',
     description='Simple but powerful management for complex class hierarchies',
     classifiers=[
         'Development Status :: 3 - Alpha',
@@ -11,6 +11,8 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: Implementation :: PyPy',
     ],
